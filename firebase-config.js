06829a55f52ad5e55522ec3a5eedb6968d7c1dac
John Ramil Mapatac <johnmapatac@yandex.ru>
2026-02-12 04:11:29 +0800

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890"
+  apiKey: "AIzaSyCrLMFITxHACl7aI-6Eda2MoSdVF228iMs",
+  authDomain: "budget-tracker-73242.firebaseapp.com",
+  projectId: "budget-tracker-73242",
+  storageBucket: "budget-tracker-73242.firebasestorage.app",
+  messagingSenderId: "1007894512742",
+  appId: "1:1007894512742:web:c9cac7a2dc1dd937a999a7"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
